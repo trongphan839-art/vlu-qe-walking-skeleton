a77814ef2946a6c3b628882ad1a5eeb16828e958
trongphan839-art <trongphan839@gmail.com>
2026-09-17 16:05:22 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 // login.js
 function login(username, password) {
-    if (username === 'admin' && password === '123') {
+    if (username === 'admin' && password === '1234') {
         return true;
     }
     return false;
